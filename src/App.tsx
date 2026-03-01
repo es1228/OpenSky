2f44b4530624ca2e1317234e6d7c6b9af2bd46ce
Evan Sidhu <129010227+es1228@users.noreply.github.com>
2026-03-01 17:05:55 -0500

    remove alerts from settings page

--- src/App.tsx
+++ src/App.tsx
@@ -514,7 +514,7 @@ export default function App() {
                     />
                 </div>
                 <div className="mx-4 mt-20 flex flex-col gap-4 md:ml-60">
-                    {pageType !== "Radar" && (
+                    {!(pageType === "Radar" || pageType === "Settings") && (
                         <Alert weatherData={weatherData} units={units} />
                     )}
                     {content}
