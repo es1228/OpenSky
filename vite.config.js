12ffc9b4cd2e4241a130b28d0e29b44c5568b2c8
Evan Sidhu <129010227+es1228@users.noreply.github.com>
2026-03-07 14:52:15 -0500

    pwa fixes

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,69 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({
-    registerType: "autoUpdate",
-    manifest: {
-      name: "OpenSky",
-      icons: [
-        {
-          src: "partly-cloudy-day.svg",
-          sizes: "any"
-        }
-      ]
-    }
-  })],
-  base: "/OpenSky/"
-})
+	plugins: [
+		react(),
+		tailwindcss(),
+		VitePWA({
+			registerType: "autoUpdate",
+			injectRegister: "auto",
+			includeAssets: [
+				"screenshot_desktop.png",
+				"screenshot_mobile.png",
+				"partly-cloudy-day.svg",
+				"appicon.png",
+			],
+			manifest: {
+				name: "OpenSky (Weather Application)",
+				short_name: "OpenSky",
+				description: "An open source weather application",
+				id: "/OpenSky/",
+        start_url: "/OpenSky/",
+				display: "fullscreen",
+				orientation: "portrait",
+				background_color: "#000",
+				screenshots: [
+					{
+						src: "screenshot_desktop.png",
+						form_factor: "wide",
+						type: "image/png",
+						sizes: "1919x908",
+						label: "Desktop View",
+					},
+					{
+						src: "screenshot_mobile.png",
+						form_factor: "narrow",
+						type: "image/png",
+						sizes: "447x799",
+						label: "Mobile View",
+					},
+				],
+				icons: [
+          {
+						src: "appicon.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+          {
+						src: "appicon.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+					{
+						src: "partly-cloudy-day.svg",
+            sizes: "512x512",
+						type: "image/svg+xml",
+						purpose: "any",
+					},
+				],
+			},
+		}),
+	],
+	base: "/OpenSky/",
+});
