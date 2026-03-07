770a30cb39d41a00f6a702828adb40f2cb430420
Evan Sidhu <129010227+es1228@users.noreply.github.com>
2026-03-07 12:15:51 -0500

    initial pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: "autoUpdate",
+    manifest: {
+      name: "OpenSky",
+      icons: [
+        {
+          src: "partly-cloudy-day.svg",
+          sizes: "any"
+        }
+      ]
+    }
+  })],
   base: "/OpenSky/"
 })
